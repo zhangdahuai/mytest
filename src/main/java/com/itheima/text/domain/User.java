5aaf6ca23bf3cab3f00806f16c4516a16553f163
ZhangDaHuai <906148605@qq.com>
2020-11-03 20:34:11 +0800

    第一次修改

--- src/main/java/com/itheima/text/domain/User.java
+++ src/main/java/com/itheima/text/domain/User.java
@@ -5,7 +5,7 @@ public class User {
     private Integer id;
     private String username;
     private String password;
-
+    private String name;
 
     public Integer getId() {
         return id;
